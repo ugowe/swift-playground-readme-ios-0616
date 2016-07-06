2e0de03080ea4f4ff8f08fe01670b84eade4d5fd
ugowe <joe@spheares.com>
2016-07-06 10:05:49 -0400

    add code for Playground lab

--- Playgrounds.playground/Contents.swift
+++ Playgrounds.playground/Contents.swift
@@ -1,2 +1,23 @@
-let str = "Hello, world"
+var str = "Hello, world"
 str = "Goodbye, world"
+
+func fizzbuzz(number: Int) -> String {
+    let fizz = number % 3
+    let buzz = number % 5
+    
+    if fizz == 0 && buzz == 0 {
+        return("FizzBuzz")
+    } else if fizz == 0 {
+        return("Fizz")
+    } else if buzz == 0 {
+        return("Buzz")
+    } else {
+        return("\(number)")
+    }
+    
+}
+
+
+for number in 1...100 {
+    print(fizzbuzz(number))
+}
